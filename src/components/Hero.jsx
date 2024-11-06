0617994b3ff525539bd96053f53332857eac65c9
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-06 22:29:19 +0300

    useTypewriter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,20 @@
+import { useTypewriter, Cursor } from "use-typewriter-hook"
+import photo_1  from '../assets/IMG_20240309_181304_597.jpg'
 function Hero() {
+    const targetText = "Hey ! I'am Estifanos a Full Stack Web Developer Based in Ethiopia";
+    const { textValue: typedText, wrapperClassName } = useTypewriter({
+      targetText: targetText,
+      typingDelayMillis: 100,
+    });
   return (
     <div className="hero">
-        <div className="hero-left">
-            
+        <div className="hero-left" >
+            <span style={{fontSize:'100px', textAlign:'center'}}>
+                {typedText}
+            </span>    
         </div>
         <div className="hero-right">
-
+                <img src={photo_1} alt="" />
         </div>
     </div>
   )
